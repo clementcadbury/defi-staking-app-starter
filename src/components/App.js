5c8926fe3a4eccaed033d35d273a15d02cb39667
clemcadbury <clementcadbury@gmail.com>
2023-04-11 15:36:00 +0200

    update

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class App extends Component {
+    // react code goes here
+    render() {
+        return (
+            <div className='text-center'>
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+}
+
+export default App
