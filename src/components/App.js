295f210bca1e54820add3db9caaff394ad58ab2c
clemcadbury <clementcadbury@gmail.com>
2023-04-12 18:22:40 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,62 @@
 import React, {Component} from 'react'
 import './App.css'
+import Navbar from './Navbar.js'
+import Web3 from 'web3'
 
 class App extends Component {
     // react code goes here
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+
+        if ( window.ethereum ) {
+            try {
+                window.web3 = new Web3(window.ethereum)
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                // connect successful
+            } catch (error) {
+                if (error.code === 4001) {
+                  // User rejected request
+                  //window.alert('Need to authorize Metamask')
+                }
+            }
+        } else {
+            //window.alert('Need metamask')
+        }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = (await web3.eth.getAccounts())[0]
+        this.setState({account:account})
+        //console.log(account)
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            account: '0x0',
+            tether: {},
+            rwd: {},
+            decentralizedBank: {},
+            tetherBalance: '0',
+            rwdBalance: '0',
+            stakingBalance: '0',
+            loading: true
+        }
+    }
+
     render() {
         return (
-            <div className='text-center'>
-                <h1>Hello World!</h1>
+            <div>
+                <Navbar account={this.state.account} />
+                <div className='text-center green'>
+                    <h1>Hello World!</h1>
+                </div>
             </div>
         )
     }
