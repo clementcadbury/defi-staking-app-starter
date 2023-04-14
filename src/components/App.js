5336d097026e8eb1fda662cbf8a33b22aa094ea6
clemcadbury <clementcadbury@gmail.com>
2023-04-14 12:49:44 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css'
 import Navbar from './Navbar.js'
+import Main from './Main.js'
 import Web3 from 'web3'
 import Tether from '../truffle_abis/Tether.json'
 import RWD from '../truffle_abis/RWD.json'
@@ -32,7 +33,7 @@ class App extends Component {
         }
     }
 
-    async loadBlockchainData() {
+    async loadBlockchainData() { 
         const web3 = window.web3
         const account = (await web3.eth.getAccounts())[0]
         this.setState({account:account})
@@ -47,7 +48,7 @@ class App extends Component {
             this.setState({tether})
             let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
             this.setState({tetherBalance: tetherBalance.toString() })
-            console.log({tetherBalance: tetherBalance})
+            //console.log({tetherBalance: tetherBalance})
         } else {
             window.alert('Tether contract not found on network ' + networkId)
         }
@@ -59,7 +60,7 @@ class App extends Component {
             this.setState({rwd})
             let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
             this.setState({rwdBalance: rwdBalance.toString() })
-            console.log({rwdBalance: rwdBalance})
+            //console.log({rwdBalance: rwdBalance})
         } else {
             window.alert('Reward token RWD contract not found on network ' + networkId)
         }
@@ -69,14 +70,39 @@ class App extends Component {
         if (decentralBankData){
             const decentralBank = new web3.eth.Contract(DecentralBank.abi,decentralBankData.address)
             this.setState({decentralBank})
-            let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
+            const stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
             this.setState({stakingBalance: stakingBalance.toString() })
-            console.log({stakingBalance: stakingBalance})
+            //console.log({stakingBalance: stakingBalance})
         } else {
             window.alert('Decentral Bank contract not found on network ' + networkId)
         }
 
+        if (decentralBankData && tetherData){
+            const tetherAllowance = await this.state.tether.methods.allowance(this.state.account,decentralBankData.address).call()
+            this.setState({tetherAllowance: tetherAllowance.toString() })
+        }
+
         this.setState({loading: false})
+    }
+
+    async balances() {
+        
+    }
+
+    stakeTokens = (amount) => {
+        this.setState({loading: true})
+        this.state.tether.methods.approve(this.state.decentralBank._address,amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+            this.state.decentralBank.methods.depositTokens(amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+                this.setState({loading: false})
+            })
+        })
+    }
+
+    unstakeTokens = () => {
+        this.setState({loading: true})
+        this.state.decentralBank.methods.unstakeTokens().send({from: this.state.account}).on('transactionHash', (hash) => {
+            this.setState({loading: false})
+        })
     }
 
     constructor(props){
@@ -87,6 +113,7 @@ class App extends Component {
             rwd: {},
             decentralBank: {},
             tetherBalance: '0',
+            tetherAllowance: '0',
             rwdBalance: '0',
             stakingBalance: '0',
             loading: true
@@ -94,12 +121,29 @@ class App extends Component {
     }
 
     render() {
+        let content = this.state.loading ? 
+            <p id='loader' className='text-center' style={{margin:'30px'}}>LOADING PLEASE...</p>
+            :
+            <Main 
+                tetherBalance={this.state.tetherBalance}
+                rwdBalance={this.state.rwdBalance}
+                stakingBalance={this.state.stakingBalance}
+                tetherAllowance={this.state.tetherAllowance}
+                stakeTokens={this.stakeTokens}
+                unstakeTokens={this.unstakeTokens}
+            />
+        
         return (
-            <div>
+            <div className='App' style={{position:'relative',backgroundColor:'#01113d'}}>
                 <Navbar account={this.state.account} />
-                <div className='text-center green' style={{marginTop:'50px'}} >
-                    {console.log(this.state.loading)}
-                    <h1>Hello World!</h1>
+                <div className='container-fluid mt-5' >
+                    <div className='row'>
+                        <main role='main' className='col-lg-12 mx-auto' style={{maxWidth:'600px',minHeight:'100vh'}}>
+                            <div>
+                                {content}
+                            </div>
+                        </main>
+                    </div>
                 </div>
             </div>
         )
